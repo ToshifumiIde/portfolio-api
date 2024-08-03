8a094c86f9da7503a9342254f32e7eb901aaed8c
井手俊文 <toshifumiide198765@gmail.com>
2024-08-03 15:21:27 +0900

    create Controller Service Repository Mapper

--- src/main/java/portfolioapi/portfolioapi/controller/test/TestsApiImpl.java
+++ src/main/java/portfolioapi/portfolioapi/controller/test/TestsApiImpl.java
@@ -1,15 +1,23 @@
 package portfolioapi.portfolioapi.controller.test;
 
+import lombok.RequiredArgsConstructor;
+import org.apache.ibatis.javassist.NotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 import portfolioapi.portfolioapi.controller.test.model.TestDto;
 import portfolioapi.portfolioapi.controller.test.model.TestRegistrationDto;
 import portfolioapi.portfolioapi.controller.test.model.TestRow;
+import portfolioapi.portfolioapi.model.Test;
+import portfolioapi.portfolioapi.service.TestService;
 
 
 @RestController
+@RequiredArgsConstructor
 public class TestsApiImpl implements TestsApi {
+
+  private final TestService testService;
+
   @Override
   public ResponseEntity<Void> createTest(TestRegistrationDto testRegistrationDto) {
     return new ResponseEntity<>(HttpStatus.OK);
@@ -21,8 +29,16 @@ public class TestsApiImpl implements TestsApi {
   }
 
   @Override
-  public ResponseEntity<TestDto> getTest(String uuid) {
-    return new ResponseEntity<>(HttpStatus.OK);
+  public ResponseEntity<TestDto> getTest(String uuid) throws NotFoundException {
+
+    Test entity = testService.getByUuid(uuid);
+
+    TestDto dto = new TestDto();
+    dto.setUuid(entity.getUuid());
+    dto.setTitle(entity.getTitle());
+    dto.setDescription(entity.getDescription());
+
+    return new ResponseEntity<>(dto, HttpStatus.OK);
   }
 
   @Override
---
+++ src/main/java/portfolioapi/portfolioapi/dao/TestMapper.java
@@ -0,0 +1,8 @@
+package portfolioapi.portfolioapi.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+import portfolioapi.portfolioapi.repository.TestRepository;
+
+@Mapper
+public interface TestMapper extends TestRepository {
+}
